12327110a1a36502cbd15727b2d48746ad67c568
Juan Manuel Salazar <134428143+Juan8516@users.noreply.github.com>
2024-06-19 07:10:58 -0500

    feat: crear componente para totales

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,22 @@
+export default function OrderTotals() {
+  return (
+    <>
+        <div>
+            <h2>Total y Propina</h2>
+            <p>Subtotal a pagar: {''}
+                <span>$0</span>
+            </p>
+
+            <p>¨Propina: {''}
+                <span>$0</span>
+            </p>
+
+            <p>Total a pagar: {''}
+                <span>$0</span>
+            </p>
+        </div>
+
+        <button></button>
+    </>
+  )
+}
